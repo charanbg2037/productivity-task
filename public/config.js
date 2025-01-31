bd3f2fc73d409c57ee3131739b16cbaef31ab337
charanbg2037 <charanbg2037@gmail.com>
2025-01-31 13:27:30 +0530

    fourth small4 commit

--- public/config.js
+++ public/config.js
@@ -1,11 +1,15 @@
-let API_BASE_URL = "http://localhost:3000"; // Default value
+const API_BASE_URL = fetch("https://your-backend-server.com/api/config");
 
-fetch("/api/config")
-    .then(response => response.json())
-    .then(config => {
-        API_BASE_URL = config.BACKEND_URL;
-        console.log("Loaded API_BASE_URL:", API_BASE_URL);
-    })
-    .catch(error => console.error("Error loading config:", error));
+///  // Default value
+
+// fetch("/api/config")
+//     .then(response => response.json())
+//     .then(config => {
+//         API_BASE_URL = config.BACKEND_URL;
+//         console.log("Loaded API_BASE_URL:", API_BASE_URL);
+//     })
+//     .catch(error => console.error("Error loading config:", error));
+// fetch("https://your-backend-server.com/api/config")
+
 
 export { API_BASE_URL };
